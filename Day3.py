38974b221cfca743737f82e789bda75f208b9413
Mehrnaz <mehrnaz.nemati2000@gmail.com>
2025-01-03 14:10:46 -0500

    improved code

--- Day3.py
+++ Day3.py
@@ -2,12 +2,29 @@ height = int(input("Please enter your height: "))
 
 if height >= 120:
     age = int(input("Please enter your age: "))
-    print("You can ride.")
-    if age > 18 :
-        print("You have to pay 12$")
-    elif 12 < age <= 18:
-        print("You have to pay 7$")
-    else:
-        print("You have to pay 5$.")
+    photo = input("Do you want a photo? Y or N")
+    cost = 0
+    if photo == "Y":
+        print("You can ride.")
+        if age < 12 :
+            cost += 5 + 3
+            print(f"You have to pay {cost}$")
+        elif 12 <= age <= 18:
+            cost += 7 + 3
+            print(f"You have to pay {cost}$")
+        else:
+            cost += 12 + 3
+            print(f"You have to pay {cost}$.")
+    elif photo == "N":
+        print("You can ride.")
+        if age < 12:
+            cost += 5
+            print(f"You have to pay {cost}$")
+        elif 12 <= age <= 18:
+            cost += 7
+            print(f"You have to pay {cost}$")
+        else:
+            cost += 12
+            print(f"You have to pay {cost}$.")
 else:
     print("You can't ride.")
